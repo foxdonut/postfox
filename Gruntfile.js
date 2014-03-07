e586e5d43eef812a7fd2af38b8665d5427600842
Freddy Daoud <freddy@Freddys-MacBook-Pro.local>
2014-03-07 18:09:35 -0500

    First Mocha test.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,16 @@
 module.exports = function(grunt) {
+  grunt.loadNpmTasks("grunt-mocha-test");
+
   grunt.initConfig({
+    mochaTest: {
+      test: {
+        options: {
+          reporter: "spec"
+        },
+        src: ["test/**/*.js"]
+      }
+    }
   });
 
-  grunt.registerTask('test', []);
+  grunt.registerTask("test", "mochaTest");
 };
--- test/postfox-tests.js
+++ test/postfox-tests.js
@@ -1,5 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var assert = require("assert");
+
+describe("Array", function() {
+  describe("#indexOf()", function() {
+    it("should return -1 when the value is not present", function() {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
+});
