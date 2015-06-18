86ffb3229cc4d94b4e1b52d53ef5174e410bc826
foxdonut <fdaoud@fastmail.com>
2015-06-17 22:05:00 -0400

    Use chai.expect for tests.

--- test/postfox-tests.js
+++ test/postfox-tests.js
@@ -1,4 +1,5 @@
-var assert = require("assert");
+var expect = require("chai").expect;
+
 var _ = require("lodash");
 
 var postfox = require("../src/postfox");
@@ -17,19 +18,19 @@ var plus = postfox.plus,
 
 describe("postfox", function() {
   it("uses function length", function() {
-    assert.equal(14, postfox(5, 1, 2, plus, 4, times, plus, 3, minus));
+    expect(postfox(5, 1, 2, plus, 4, times, plus, 3, minus)).to.equal(14);
   });
 
   it("uses quotations and function arity from helper", function() {
-    assert.equal(15, postfox([1, 2, 3, 4, 5], qtn(plus), 0, arity(3, _.reduce)));
+    expect(postfox([1, 2, 3, 4, 5], qtn(plus), 0, arity(3, _.reduce))).to.equal(15);
   });
 
   it("does partial function application in quotation", function() {
-    assert.deepEqual([10, 20, 30, 40, 50], postfox([1, 2, 3, 4, 5], qtn(10, times), arity(2, _.map)));
+    expect(postfox([1, 2, 3, 4, 5], qtn(10, times), arity(2, _.map))).to.deep.equal([10, 20, 30, 40, 50]);
   });
 
   it("does multiple partial function application in quotation", function() {
-    assert.deepEqual([16, 20, 24, 28, 32], postfox([1, 2, 3, 4, 5], qtn(3, plus, 4, times), arity(2, _.map)));
+    expect(postfox([1, 2, 3, 4, 5], qtn(3, plus, 4, times), arity(2, _.map))).to.deep.equal([16, 20, 24, 28, 32]);
   });
 
   it("allows postfox function definition", function() {
@@ -40,18 +41,18 @@ describe("postfox", function() {
     var avg2 = postfunc(plus, 2, divide);
     var avg3 = postfunc(avg2, avg2);
 
-    assert.equal(42, postfox(80, 4, avg1));
-    assert.equal(42, postfox(80, 4, avg2));
-    assert.equal(27, postfox(2, 80, 4, avg2, plus, 10, avg2));
-    assert.equal(40, postfox(10, 60, 80, avg3));
+    expect(postfox(80, 4, avg1)).to.equal(42);
+    expect(postfox(80, 4, avg2)).to.equal(42);
+    expect(postfox(2, 80, 4, avg2, plus, 10, avg2)).to.equal(27);
+    expect(postfox(10, 60, 80, avg3)).to.equal(40);
   });
 
   it("provides utility functions", function() {
-    assert.equal(42, postfox(84, 2, divide));
-    assert.equal(5, postfox("hello", lengthOf));
-    assert.equal(true, postfox("8", 8, equal));
-    assert.equal(false, postfox("8", 8, equ));
-    assert.equal(true, postfox((4+4), 8, equ));
-    assert.deepEqual([2, 4, 42], postfox(2, [4, 42], cons));
+    expect(postfox(84, 2, divide)).to.equal(42);
+    expect(postfox("hello", lengthOf)).to.equal(5);
+    expect(postfox("8", 8, equal)).to.equal(true);
+    expect(postfox("8", 8, equ)).to.equal(false);
+    expect(postfox((4+4), 8, equ)).to.equal(true);
+    expect(postfox(2, [4, 42], cons)).to.deep.equal([2, 4, 42]);
   });
 });
